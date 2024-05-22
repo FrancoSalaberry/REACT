bf6590bb974f75e01421b37dc7cb56565acac3ca
FrancoSalaberry <francosalaberry98@gmail.com>
2024-05-22 17:25:49 -0300

    #Componentes

---
+++ ecommercesala/src/components/indumentaria/indumentaria.jsx
@@ -0,0 +1,10 @@
+const Indumentaria = ({tipe, description}) => {
+    return(
+        <div>
+            <p>{tipe}</p>
+            <p>descripcion: {description}</p>
+        </div>
+    )
+}
+
+export default Indumentaria
